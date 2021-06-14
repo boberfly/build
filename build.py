0363fa4bb1af78559d05c154341e95b531dc88bc
John Haddon <thehaddonyoof@gmail.com>
2021-06-14 14:45:33 +0100

    build.py : Fix container builds with non-default `--organisation`

--- build.py
+++ build.py
@@ -275,7 +275,7 @@ if args.docker and not os.path.exists( "/.dockerenv" ) :
 	if args.interactive :
 		containerCommand = "env {env} bash".format( env = containerEnv )
 	else :
-		containerCommand = "env {env} bash -c '/build.py --project {project} --version {version} --pythonVariant {pythonVariant} --upload {upload}'".format( env = containerEnv, **formatVariables )
+		containerCommand = "env {env} bash -c '/build.py --organisation {organisation} --project {project} --version {version} --pythonVariant {pythonVariant} --upload {upload}'".format( env = containerEnv, **formatVariables )
 
 	dockerCommand = "docker run -it {mounts} --name {name} {image}-run {command}".format(
 		mounts = containerMounts,
