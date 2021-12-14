ff1d5f6ec451cddba42c78f24132f54304369610
John Haddon <thehaddonyoof@gmail.com>
2021-12-14 12:50:07 +0000

    Bump version to 2.0.0

--- build.py
+++ build.py
@@ -50,7 +50,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
 	"--build-env-version",
 	dest = "buildEnvVersion",
-	default = "1.3.0",
+	default = "2.0.0",
 	help = "The container image tag to use for docker builds."
 )
 
